37fd3e977f730d28f1e68f33b3ea7e14d453087e
Zander Baker <zanderbaker100@gmail.com>
2023-08-12 20:46:17 -0700

    Create dummy CLI app

---
+++ MacroMat.CLI/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandDotNet;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        return new AppRunner<Program>().Run(args);
+    }
+
+    [Command("run")]
+    public int Run(string file)
+    {
+        if (!RequireFileExists(file))
+            return 1;
+
+        return 0;
+    }
+
+    [Command("debug")]
+    public int Debug(string file)
+    {
+        if (!RequireFileExists(file))
+            return 1;
+
+        return 0;
+    }
+
+    private bool RequireFileExists(string file)
+    {
+        if (!File.Exists(file))
+        {
+            file = Path.Join(Directory.GetCurrentDirectory(), file);
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"The given file ({file}) was not found.");
+                
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
