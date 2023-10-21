2a744f740379adb25a58a27c4a66cfea13864afb
Zander Baker <zanderbaker100@gmail.com>
2023-10-20 21:27:33 -0700

    Add xmldoc to IKeyRepresentation

--- MacroMat/Common/IKeyRepresentation.cs
+++ MacroMat/Common/IKeyRepresentation.cs
@@ -1,5 +1,10 @@
 ﻿namespace MacroMat.Common;
 
+/// <summary>
+/// Unifying interface for <see cref="VirtualKey"/> and <see cref="Scancode"/>.
+/// Classes that implement this interface other than VirtualKey and Scancode
+/// are moot when used with existing library methods.
+/// </summary>
 public interface IKeyRepresentation
 {
     
